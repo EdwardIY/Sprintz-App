419481cefabdb87105adf589930592d9101a724d
Edward Ibeh-Young <edward.ibehyoung@gmail.com>
2023-05-29 17:32:57 -0700

    added addition card details/and styles

--- src/comps/Tasks.tsx
+++ src/comps/Tasks.tsx
@@ -12,104 +12,62 @@ export default function Tasks() {
         </div> */}
   
         <div className="Task Task--group Container--col">
-          <span className="TaskCategory">From Group 2</span>
+          <span className="TaskCategory">From Group <span>'Health'</span></span>
           <span className="TaskDescription"> Workout at 10am</span>
+
+          <div className="Container--col TaskInfo">
           <span className="TaskControls Container--row">
             <Icon.Pencil />
             <Icon.Check2 />
             <Icon.XLg />
-          </span>
+            </span>
+            <span className='TaskTime'> Due in 2day(s) an 12hr(s)</span>
+          </div>
+
         </div>
         <div className="Task Task--group Container--col">
-          <span className="TaskCategory">From Group 4</span>
-          <span className="TaskDescription"> Clean the garden at 6pm</span>
+          <span className="TaskCategory">From Group <span>'Yard Work'</span></span>
+          <span className="TaskDescription"> Clean the garden</span>
+
+          <div className="Container--col TaskInfo">
           <span className="TaskControls Container--row">
             <Icon.Pencil />
             <Icon.Check2 />
             <Icon.XLg />
-          </span>
+            </span>
+            <span className='TaskTime'> Due in 1day(s)</span>
+          </div>
+
         </div>
         <div className="Task Task--sprint Container--col">
-          <span className="TaskCategory">From Sprint 2</span>
-          <span className="TaskDescription"> Complete Brochure</span>
+          <span className="TaskCategory">From Sprint <span>'Learn Algorithms'</span></span>
+          <span className="TaskDescription">Finish DPS Youtube video</span>
+
+          <div className="Container--col TaskInfo">
           <span className="TaskControls Container--row">
             <Icon.Pencil />
             <Icon.Check2 />
             <Icon.XLg />
-          </span>
-        </div>
-        <div className="Task Task--group Container--col">
-          <span className="TaskCategory">From Sprint 2</span>
-          <span className="TaskDescription"> Learn AVL Trees and after review BST. Finished rough draft of spacex x clon by making the note dynamic. After that you can host it on your port with nelify. I really need to study flashcards</span>
-          <span className="TaskControls Container--row">
-            <Icon.Pencil />
-            <Icon.Check2 />
-            <Icon.XLg />
-          </span>
+            </span>
+            <span className='TaskTime'> Due in 12hr(s)</span>
+          </div>
+
         </div>
         <div className="Task Task--sprint Container--col">
-          <span className="TaskCategory">From Sprint 2</span>
-          <span className="TaskDescription"> Complete Brochure</span>
+          <span className="TaskCategory">From Sprint <span>'Learn Algorithms'</span></span>
+          <span className="TaskDescription">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga praesentium fugit sed est incidunt id recusandae aspernatur assumenda nulla eos consectetur, dolor quibusdam dolores, quo earum laborum eligendi fugiat voluptatem!</span>
+
+          <div className="Container--col TaskInfo">
           <span className="TaskControls Container--row">
             <Icon.Pencil />
             <Icon.Check2 />
             <Icon.XLg />
-          </span>
+            </span>
+            <span className='TaskTime'> Due in 12hr(s)</span>
+          </div>
+
         </div>
-        <div className="Task Task--sprint Container--col">
-          <span className="TaskCategory">From Sprint 2</span>
-          <span className="TaskDescription"> Complete Brochure</span>
-          <span className="TaskControls Container--row">
-            <Icon.Pencil />
-            <Icon.Check2 />
-            <Icon.XLg />
-          </span>
-        </div>
-        <div className="Task Task--sprint Container--col">
-          <span className="TaskCategory">From Sprint 2</span>
-          <span className="TaskDescription"> Complete Brochure</span>
-          <span className="TaskControls Container--row">
-            <Icon.Pencil />
-            <Icon.Check2 />
-            <Icon.XLg />
-          </span>
-        </div>
-        <div className="Task Task--sprint Container--col">
-          <span className="TaskCategory">From Sprint 2</span>
-          <span className="TaskDescription"> Complete Brochure</span>
-          <span className="TaskControls Container--row">
-            <Icon.Pencil />
-            <Icon.Check2 />
-            <Icon.XLg />
-          </span>
-        </div>
-        <div className="Task Task--sprint Container--col">
-          <span className="TaskCategory">From Sprint 2</span>
-          <span className="TaskDescription"> Complete Brochure</span>
-          <span className="TaskControls Container--row">
-            <Icon.Pencil />
-            <Icon.Check2 />
-            <Icon.XLg />
-          </span>
-        </div>
-        <div className="Task Task--sprint Container--col">
-          <span className="TaskCategory">From Sprint 2</span>
-          <span className="TaskDescription"> Complete Brochure</span>
-          <span className="TaskControls Container--row">
-            <Icon.Pencil />
-            <Icon.Check2 />
-            <Icon.XLg />
-          </span>
-        </div>
-        <div className="Task Task--sprint Container--col">
-          <span className="TaskCategory">From Sprint 2</span>
-          <span className="TaskDescription"> Complete Brochure</span>
-          <span className="TaskControls Container--row">
-            <Icon.Pencil />
-            <Icon.Check2 />
-            <Icon.XLg />
-          </span>
-        </div>
+        
       </div>
     )
   }
