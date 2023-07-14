172d8a943b0681d37ca3995367aaee0ae761af3f
Edward Ibeh-Young <edward.ibehyoung@gmail.com>
2023-07-14 14:01:41 -0700

    Sign in and Sign Up without firebase are done, also all test for account creation

--- src/Pages/SignIn.tsx
+++ src/Pages/SignIn.tsx
@@ -8,7 +8,7 @@ export default function SignIn() {
         <input className='SignIn_Form_Input' type="text" placeholder="Enter your username" />
         <input className='SignIn_Form_Input' type="password" placeholder="Enter your password" />
         <div className='Btn a'>Sign In</div>
-        <div className="Btn b">Create Account</div>
+        <a href="/sign-up" className="Btn b"> Sign Up</a>
     </form>
 </section>
 }
--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,23 +1,86 @@
-import '../styles/SignUp.css'
+import { useState } from 'react';
+import '../styles/SignUp.css';
+import * as Icon from 'react-bootstrap-icons';
 
 export default function SignUp() {
+    const [createAccount, setCreateAccount] = useState(false);
+    const [hasPassword, setHasPassword] = useState(false);
+    const [details, setDetails] = useState({});
+
+
+    const verifyPassword = (value: string) => {
+        if (value.length) setHasPassword(true);
+        else setHasPassword(false)
+    }
+
+    const handleCreate = (e:any) => {
+        e.preventDefault();
+
+
+        const Test1 = Test_Username(e.target.username.value)
+        const Test2 = Test_Password1(e.target.password1.value)
+        const Test3 = Test_Password2(e.target.password2.value,e.target.password1.value)
+        
+        if (Test1 !== 'passed') return alert(Test1)
+        if (Test2 !== 'passed') return alert(Test2)
+        if (Test3 !== 'passed') return alert(Test3)
+
+        alert('All Passed')
+        // create User
+
+    }
+
     return <section className="SignUp Container--col">
                 <h1 className="Title">Join Sprintz</h1>
         
                 <div className='Btn a'>Join With Google</div>
                 <div>or</div>
-                <div className="Btn b">Create Account</div>
+                <div onClick={ () => setCreateAccount(true)} className="Btn b">Create Account</div>
                 
-                <span className='Note'>Already have an account? <span>Sign In</span></span>
+                <span className='Note'>Already have an account? <a href='/'>Sign In</a></span>
             {/* <form className='SignIn_Form Container--col'>
                 <input className='SignIn_Form_Input' type="text" placeholder="Enter your username" />
                 <input className='SignIn_Form_Input' type="password" placeholder="Enter your password" />
             </form> */}
-        <form className='Container--col'>
-            <input type="text" placeholder='Create username'/>
-            <input type="password" placeholder='Create password'/>
-            <input disabled type="password" placeholder='Retype password' />
-            <div>Create Account</div>
-        </form>
+        
+        {createAccount &&  <form onSubmit={(e) => handleCreate(e)} className='SignUp_Form Container--col'>
+            <Icon.XLg className='SignUp_Form_Close' onClick={ () => setCreateAccount(false) } />
+            <h2>Create Account</h2>
+            <input name='username' className='SignUp_Form_Input' type="text" placeholder='Create username' required/>
+            <input onChange={(e) => verifyPassword(e.target.value)}  name='password1'  className='SignUp_Form_Input' type="password" placeholder='Create password' required/>
+            <input style={{display: hasPassword ? 'block' : 'none'}} name='password2'  className='SignUp_Form_Input' type="password" placeholder='Retype password' required />
+            <button  className="Btn b" >Create Account</button>
+        </form>}
+
     </section>
+}
+
+function Test_Username(name:string) {
+    const chars = 'abcdefghijklmnopqrstuvwxyz1234567890'
+
+
+    if( !isNaN(+name[0])) return 'Username cannot start with a number'
+
+    for (let char of name) {
+        if(!chars.includes(char.toLowerCase())) return 'Username contains invalid characters'
+    }
+    return 'passed';
+}
+function Test_Password1(password:string) {
+    const chars = 'abcdefghijklmnopqrstuvwxyz1234567890#$'
+    for (let char of password) {
+        if(char === ' ') return 'Password cannot conatin spaces'
+        if(!chars.includes(char.toLowerCase())) return 'Password contains invalid characters'
+    }
+    return 'passed'
+
+}
+function Test_Password2(a:string,b:string) {
+    if (a.length != b.length) return 'Passwords do not match'
+    
+    for (let i = 0; i < a.length; i++){
+        if (a[i] !== b[i]) return 'Passwords do not match';
+    }
+
+    return 'passed'
 }
